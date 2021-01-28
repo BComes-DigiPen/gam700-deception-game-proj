674b82d05bb6ad52bac6eaf70af7ebbb3c0b3fee
Acoliversa <56551845+Acoliversa@users.noreply.github.com>
2021-01-27 18:05:17 -0800

    NPCController moves NPC randomly

--- GAM 7.0.0 ''Deception'' Game Project/Assets/Scripts/NPCController.cs
+++ GAM 7.0.0 ''Deception'' Game Project/Assets/Scripts/NPCController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
+    public enum State { Standing, Walking };
+    public State currentState = State.Standing;
+    public float timer = 0;
+    public float speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,29 @@ public class NPCController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer <= 0)
+        {
+            float temp = Random.Range(25, 150) / 100.0f;
+            timer = temp * temp;
+            //print(temp + ", " + temp * temp);
+            ToggleState();
+            if (currentState == State.Standing)
+            {
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+            }
+            else
+            {
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero * speed;
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void ToggleState ()
+    {
+        currentState = currentState == State.Standing ? State.Walking : State.Standing;
     }
 }
