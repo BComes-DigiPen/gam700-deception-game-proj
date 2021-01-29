196adbba65eb484ade846d0f8421bc0262f73244
Remmie <emily@isimar.com>
2021-01-28 18:04:39 -0800

    Added pause and menu transition functions to gamemanager as well as switches for game states, not tested yet

--- GAM 7.0.0 ''Deception'' Game Project/Assets/Scripts/GameManager.cs
+++ GAM 7.0.0 ''Deception'' Game Project/Assets/Scripts/GameManager.cs
@@ -1,21 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	//public GameState GameManagerState = GameState.InMainMenu;
+    //public GameState GameManagerState = GameState.InMainMenu;
 
-	public enum GameState
-	{ InMainMenu, InGame, Paused, GunnerVictory, RunnerVictory }; // add other states as needed
+    public enum GameState
+    { InMainMenu, InGame, Paused, GunnerVictory, RunnerVictory }; // add other states as needed
 
-	// Start is called before the first frame update
-	private void Start()
-	{
-	}
+    private GameState state;
 
-	// Update is called once per frame
-	private void Update()
-	{
-	}
+    public static bool gamePaused = false;
+
+    private void Update()
+    {
+        Paused();
+        PlayGame();
+
+        switch (state)
+        {
+            case GameState.InMainMenu:
+                break;
+            case GameState.InGame:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Paused:
+                Time.timeScale = 0f;
+                break;
+        }
+    }
+
+    void Paused()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            if(gamePaused)
+            {
+                state = GameState.InGame;
+            }
+            else
+            {
+                state = GameState.Paused;
+            }
+        }
+    }
+    void PlayGame()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("SampleScene");
+            state = GameState.InGame;
+        }
+    }
 }
